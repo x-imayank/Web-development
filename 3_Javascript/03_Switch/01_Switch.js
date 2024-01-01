a39f5cff6fef9413a3ad7055d4a449d405ed3606
Mayank <mail.mayank001@gmail.com>
2024-01-01 20:21:03 +0530

    Update

---
+++ 3_Javascript/03_Switch/01_Switch.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')()
+
+let a = Number(prompt("Enter the number for days name: "));
+
+switch (a) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Enter correct number.");
+}
