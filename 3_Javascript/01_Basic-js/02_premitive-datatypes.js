6cf2bf4f827ab67ee44459cf8c8b86f7392d97c3
Mayank <mail.mayank001@gmail.com>
2024-01-01 18:03:31 +0530

    Update

---
+++ 3_Javascript/01_Basic-js/02_premitive-datatypes.js
@@ -0,0 +1,21 @@
+let a = "Datatype";
+let b = 22;
+let c = 3.55;
+const d = true;
+let e = null;
+let f = undefined;
+
+console.log(a,b,c,d,e,f)
+console.log("\n");
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f); //type of null is object not null.
+console.log("\n");
+
+let o = {
+    "name": "javascript",
+    "Job": "Developer",
+
+}
+console.log(o);
+
+o.salary = "100cr";//Adding salary in box.
+console.log(o);
