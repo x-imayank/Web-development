c6b0e18bd9500a8e1f777e237b194dd6c24aa7fa
Mayank <mail.mayank001@gmail.com>
2024-02-16 23:31:03 +0530

    First commit

--- 3_Javascript/02_Variables/Variables.js
+++ 3_Javascript/02_Variables/Variables.js
@@ -19,8 +19,17 @@ Age = 12; // Change the value of Age
 console.log(FullName, Age);
 
 const name = "Tony";
-name = "Stark" // We didnt change the value of const.
+// name = "Stark" // We didnt change the value of const.
 
 console.log("\n", name);
 
 
+{
+     let a = 5;
+     // let a = 6; // We didnt declare the same let variable in same block.
+     console.log(a);
+}
+{
+     let a = 6;
+     console.log(a);
+}
