840b840847a97ceb534856f6acf6cb6da1b26886
Mayank <mail.mayank001@gmail.com>
2024-02-16 23:16:23 +0530

    First commit

---
+++ 3_Javascript/02_Variables/Variables.js
@@ -0,0 +1,10 @@
+fullName = "Tony stark"; // Declare a variable.
+age = 24;
+price = 121;
+
+x = null;
+y = undefined;
+
+bool = false; // Boolian
+
+console.log(fullName, "\n", age, "\n", price, "\n", x, "\n", y, "\n", typeof bool);
--- 3_Javascript/02_Variables/new.js
+++
@@ -1,8 +0,0 @@
-fullName = "Tony stark"; // Declare a variable.
-age = 24;
-price = 121;
-
-x = null;
-y = undefined;
-
-console.log(fullName, "\n", age, "\n", price, "\n", x, "\n", y);
