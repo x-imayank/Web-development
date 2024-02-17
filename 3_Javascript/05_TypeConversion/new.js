62f79487816dc71699297ad54e0f3e613459b5af
Mayank <mail.mayank001@gmail.com>
2024-02-17 23:13:43 +0530

    First commit

---
+++ 3_Javascript/05_TypeConversion/new.js
@@ -0,0 +1,7 @@
+let score = "23";
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score) // Changing the type of string into nummber.
+console.log(typeof valueInNumber)
