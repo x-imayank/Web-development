15232e34117799f5e910592249e5776ff0a0ded5
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:15:11 +0530

    First commit

--- 3_Javascript/10_Number.js
+++ 3_Javascript/10_Number.js
@@ -5,3 +5,8 @@ const balance = new Number(100)
 console.log(balance)
 
 console.log(balance.toString()) // Make number to string and it contains all the datatypes of string.
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) // Precision value
+
+const num = 23.8943;
+console.log(num.toPrecision(3));
