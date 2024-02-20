ea9ea5f84a180566d2af0fd9f90b9157ca39804b
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:25:51 +0530

    First commit

--- 3_Javascript/11_Math.js
+++ 3_Javascript/11_Math.js
@@ -10,3 +10,9 @@ console.log("\n")
 console.log(Math.random())
 console.log(Math.random() * 10)
 console.log((Math.random() * 10) + 1) // Upto 10
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // After 10 all random numbers.
