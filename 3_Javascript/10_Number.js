898d63919eb5f2c1b82d1e7b96e0492346e988c7
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:11:38 +0530

    FIrst commit

---
+++ 3_Javascript/10_Number.js
@@ -0,0 +1,7 @@
+const Score = 400;
+console.log(typeof Score, Score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString()) // Make number to string and it contains all the datatypes of string.
