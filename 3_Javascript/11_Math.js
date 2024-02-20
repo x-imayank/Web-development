d238082f17d3eee3ee1ecffd79a9029a1027f625
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:18:42 +0530

    First commit

--- 3_Javascript/11_Math.js
+++ 3_Javascript/11_Math.js
@@ -1,4 +1,7 @@
 console.log(Math)
 
 console.log(Math.abs(-4)) // abs = absolute it changes the negative sign to positive sign.
-console.log(Math.round(4.76))
+console.log(Math.round(6.76))
+console.log(Math.floor(9.76))
+console.log(Math.ceil(4.76))
+
