b9ebe86baae67c02a5e602300ad7e1ec65cdb70b
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:54:03 +0530

    First commit

---
+++ 3_Javascript/11_Dates.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString())
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 23) // year,month,date
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Date.now())
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate)
