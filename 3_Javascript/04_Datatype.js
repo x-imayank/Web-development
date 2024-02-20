05543866e258c61ab3228616e11cba0addb76154
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:29:25 +0530

    First commit

---
+++ 3_Javascript/04_Datatype.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version.
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+let name = "Tony"; // String
+let age = 14 // Integer
+let isLoggedIn = false // Boolian
+
+// Datatype = it stores the range of variable.
+// types of datatype :- string, number, boolian, null, undefined, symbol, etc...
+
+console.log(typeof name) // (typeof ) tells the type of datatype 
+console.log(typeof null) // It returns object it is also known as the fault of javascipt.
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLogged = false;
+const outSideTemp = null;
+let userEmail; // It declairs undefined value.
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId)
+
+
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Refrence type (Non-premitive)= Arrays, Objects, Functions
