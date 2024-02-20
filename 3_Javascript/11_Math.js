c76b1be572f473075f58fdbb9d6e7303d025b493
Mayank <mail.mayank001@gmail.com>
2024-02-21 00:21:16 +0530

    First commit

--- 3_Javascript/11_Math.js
+++ 3_Javascript/11_Math.js
@@ -5,3 +5,8 @@ console.log(Math.round(6.76))
 console.log(Math.floor(9.76))
 console.log(Math.ceil(4.76))
 
+console.log("\n")
+// Random
+console.log(Math.random())
+console.log(Math.random() * 10)
+console.log((Math.random() * 10) + 1) // Upto 10
