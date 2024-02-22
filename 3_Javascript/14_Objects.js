3561a99eb07e52c7aafe32e58f305e2b4ce1465d
Mayank <mail.mayank001@gmail.com>
2024-02-23 00:45:42 +0530

    First commit

---
+++ 3_Javascript/14_Objects.js
@@ -0,0 +1,12 @@
+// Object literals
+
+const JsUser = {
+     name: "Javascript",
+     location: "Unknown",
+     email: "js@google.com",
+     isLoggedIn: false,
+     lastLoggedDays: ["Monday", "Tuesday"]
+}
+
+console.log(JsUser.email)
+// console.log(JsUser[email])
