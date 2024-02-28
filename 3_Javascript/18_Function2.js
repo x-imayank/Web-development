93865f1a01b926277bf41a2fe79f0610509ca97e
Mayank <mail.mayank001@gmail.com>
2024-02-29 01:10:28 +0530

    First commit

--- 3_Javascript/18_Function2.js
+++ 3_Javascript/18_Function2.js
@@ -12,3 +12,21 @@ function calculatePrice2(num1, num2, ...var1) {
      return var1;
 }
 console.log(calculatePrice2(200, 300, 100, 240)) // 200 and 300 goes with num1,num2 and 100,200 goes with ...var1;
+
+
+// Use of object in function.
+const user = {
+     username: "Javascript",
+     prices: 123
+}
+
+function object(anyobject) {
+     console.log(`username is ${anyobject.username} and price is ${anyobject.prices}`)
+}
+
+object(user)
+
+object({
+     username: "Sam",
+     prices: 399
+})
