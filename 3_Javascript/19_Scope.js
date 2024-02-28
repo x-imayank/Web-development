467da65ce8c267f49ec0a677deea4fb7fa286d15
Mayank <mail.mayank001@gmail.com>
2024-02-29 01:20:51 +0530

    First commit

---
+++ 3_Javascript/19_Scope.js
@@ -0,0 +1,19 @@
+
+{ } //Scope
+
+if (true) {
+     let a = 10
+     const b = 20
+     var c = 30
+     console.log("Inner", a)
+     console.log("Inner", c)
+
+}
+
+let a = 20
+console.log(a)
+
+var c = 12
+console.log(c)
+// console.log(b)
+// console.log(c)
