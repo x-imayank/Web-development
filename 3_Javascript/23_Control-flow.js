6545cb1594afaf96d70d775a81bd0b1edc5c9809
Mayank <mail.mayank001@gmail.com>
2024-03-07 23:32:06 +0530

    First commit

--- 3_Javascript/23_Control-flow.js
+++ 3_Javascript/23_Control-flow.js
@@ -1,6 +1,6 @@
 let val = (2 == 2)
 if (true) {
-    console.log("Yes")
+    console.log("\nYes\n")
 }
 
 if (false) {
@@ -12,7 +12,7 @@ if (false) {
 let score = 200;
 if (score < 300) {
     let power = "fly";
-    console.log(`user power: ${power}`);
+    console.log(`user power: ${power}\n`);
 }
 // console.log(`user power: ${power}`) // in let it throws an error.
 
@@ -20,4 +20,29 @@ if (score < 300) {
 
 
 const balance = 1000;
-if (balance > 500) console.log("True");
+if (balance > 500) console.log("True\n");
+
+
+// Nesting
+
+if (balance < 500) {
+    console.log("Greater")
+} else if (balance < 750) {
+    console.log("Not lesser.");
+} else if (balance == 1000) {
+    console.log("True");
+}
+
+console.log("\n")
+
+
+// Using of nesting in real life
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow for shopping.")
+} else console.log("Not allow for shopping.")
+
+
+console.log("\n")
