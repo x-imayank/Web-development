49b9d59a85046b4d8bfbb3057e309c0d9d1b2dbd
Mayank <mail.mayank001@gmail.com>
2024-03-09 02:39:50 +0530

    First commit

---
+++ 3_Javascript/26_higher-order-loop.js
@@ -0,0 +1,11 @@
+// for of loop
+
+let arr = [1, 2, 3, 4, 5, 6]
+for (const num of arr) {
+    console.log(num)
+}
+
+const name = "Hello world"
+for (const char of name) {
+    console.log(`Each character of name is ${char}`)
+}
