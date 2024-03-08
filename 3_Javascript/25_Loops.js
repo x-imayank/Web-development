22c4c64c8ac1a13e413b8daf00698c2bdfe5b2db
Mayank <mail.mayank001@gmail.com>
2024-03-09 00:25:28 +0530

    First commit

--- 3_Javascript/25_Loops.js
+++ 3_Javascript/25_Loops.js
@@ -4,3 +4,20 @@ let Arr = [1, 2, 3, 4, 12, 3]
 for (let i = 0; i <= Arr.length; i++) {
     console.log(Arr[i]);
 }
+
+// break and continue
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    console.log(i)
+}
+console.log("\n")
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        continue;
+    }
+    console.log(i)
+}
