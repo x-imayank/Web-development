efad9adfc60899dc2c95b26cb8d045cc51eb5d8c
Mayank <mail.mayank001@gmail.com>
2024-03-24 14:18:00 +0530

    First commit

--- zavascript/04_Type-of.js
+++
@@ -1,3 +0,0 @@
-console.log(typeof 123); //NUmber
-console.log(typeof 'Hey'); //String
-console.log(typeof (11 == 11)); //Boolian
--- zavascript/7_boolian.js
+++
@@ -1,5 +0,0 @@
-let a = 6;
-let b = 3;
-console.log(6 > 3);
-console.log(3 == '3'); // Check equality
-console.log(3 === '3.0'); // Value must be same (in string or integer).
